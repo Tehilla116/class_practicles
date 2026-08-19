1d7072d6b34645553be037dff5f814a7aea1051c
Tehilla Chiwama <tehillachiwama1@gmail.com>
2026-08-19 17:46:43 +0200

    first commit

---
+++ week1/Procedual.java
@@ -0,0 +1,10 @@
+public class Procedual {
+    public static void main(String[] args) {
+        String[] names = {"Ben", "Mary", "Leo"};
+        int[] ages = {10, 6, 8};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + " is " + ages[i] + " years old.");
+        }
+    }
+}
